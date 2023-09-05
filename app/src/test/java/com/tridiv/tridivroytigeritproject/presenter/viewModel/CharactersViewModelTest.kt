f437eba24a092b9cdce86002f14a5658b9f3ebd9
tridiv <roytridiv1996@gmail.com>
2023-09-05 14:50:25 +0600

    added some gradle changes and test class change

--- app/src/test/java/com/tridiv/tridivroytigeritproject/presenter/viewModel/CharactersViewModelTest.kt
+++ app/src/test/java/com/tridiv/tridivroytigeritproject/presenter/viewModel/CharactersViewModelTest.kt
@@ -6,6 +6,7 @@ import com.tridiv.tridivroytigeritproject.data.domain.ResultData
 import com.tridiv.tridivroytigeritproject.data.model.networkPojo.CharactersListResp.CharactersRespBody
 import com.tridiv.tridivroytigeritproject.data.repository.Repository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
@@ -16,30 +17,68 @@ import org.junit.Assert.*
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import retrofit2.Response
 
-
+@OptIn(ExperimentalCoroutinesApi::class)
 class CharactersViewModelTest {
 
-    private val testDispatcher = StandardTestDispatcher()
-@Mock
+
+   // private val testDispatcher = StandardTestDispatcher()
+
+    @Mock
     lateinit var repository: Repository
+
+//    @get: Rule
+//    val dispatcherRule = TestDispatcherRule()
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    private val testDispatcher = StandardTestDispatcher()
+    private lateinit var viewModel: CharactersViewModel
     @Before
     fun setUp() {
         MockitoAnnotations.openMocks(this)
         Dispatchers.setMain(testDispatcher)
+        viewModel = CharactersViewModel(repository)
     }
+
+
     @Test
-    fun getCharacters()= runTest{
+    fun `test fetching character list from api success`() = runTest {
 //       Mockito.`when`(repository.getCharactersList()).thenReturn(CharactersRespBody(null,null))
 //        val systemUnderTest = CharactersViewModel(repository)
 //        systemUnderTest.getCharactersList()
 //        testDispatcher.scheduler.advanceUntilIdle()
 //        val result = systemUnderTest.liveDataFromTesting
 //        Assert.assertEquals(0,result)
+
+        val mockResponse = ResultData.Success(Response.success(CharactersRespBody(null,null)))
+        Mockito.`when`(repository.getCharListTemp()).thenReturn(mockResponse)
+
+        viewModel.getCharactersList()
+
+        assert(viewModel.charactersDataListResponse.value != null)
+        assert(viewModel.charactersDataListResponse.value?.isEmpty() == false)
+    }
+
+
+    @Test
+    fun `test data insertion in Room DB`() = runTest {
+//       Mockito.`when`(repository.getCharactersList()).thenReturn(CharactersRespBody(null,null))
+//        val systemUnderTest = CharactersViewModel(repository)
+//        systemUnderTest.getCharactersList()
+//        testDispatcher.scheduler.advanceUntilIdle()
+//        val result = systemUnderTest.liveDataFromTesting
+//        Assert.assertEquals(0,result)
+
+        val mockResponse = ResultData.Success(Response.success(CharactersRespBody(null,null)))
+        Mockito.`when`(repository.getCharListTemp()).thenReturn(mockResponse)
+
+        viewModel.getCharactersList()
+
+        assert(viewModel.charactersDataListResponse.value != null)
+        assert(viewModel.charactersDataListResponse.value?.isEmpty() == false)
     }
 
     @After
